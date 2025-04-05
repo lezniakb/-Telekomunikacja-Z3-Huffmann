e8952d72c116c9488426bca01560943ce7f168d4
Bartosz <251574@edu.p.lodz.pl>
2025-04-05 14:57:29 +0100

    dodano menu główne

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-# initial commit
+# glowna petla
+while True:
+    print("--------------\nMenu Główne:\n"
+          "1. Skompresuj tekst\n"
+          "2. Dekompresuj tekst\n"
+          "3. Skompresuj plik\n"
+          "4. Dekompresuj plik\n"
+          "5. Pokaż statystyki\n"
+          "6. Zakończ program")
+    wybor = input("Wybierz opcję: ").strip()
+    if wybor == "1":
+        print("kompresuj tekst")
+    elif wybor == "2":
+        print("dekompresuj tekst")
+    elif wybor == "3":
+        print("kompresuj plik")
+    elif wybor == "4":
+        print("dekompresuj plik")
+    elif wybor == "5":
+        print("statystyki")
+    elif wybor == "6":
+        break
+    else:
+        print("Wybrano niewłaściwą opcję!")
+    input("Wciśnij enter aby kontynuować...")
+
+print("Program zakończony pomyślnie")
